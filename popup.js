c922a07a06a5b6a908083c84083a28d19026afcd
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2013-12-07 19:42:03 -0500

    Logs into BB, and displays user's name
    See README for To-Do

--- popup.js
+++ popup.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
-    $("#login").click(function() {
+    $("#login").click(function(event) {
+        event.preventDefault();
         if ($("#username").val() != "" && $("#password").val() != "") {
             var username = $("#username").val();
             var password = $("#password").val();
-            submitForm();
+
+            var request = $.ajax({
+                type: "post",
+                dataType:"html",
+                url: "https://my.rochester.edu/webapps/login/index",
+                data: {
+                    user_id: username,
+                    encoded_pw: window.btoa(password),
+                    encoded_pw_unicode: "."
+                },
+                success: function() {
+                    getData();
+                },
+                error: function() {
+                    alert("Error! Try Again.");
+                }
+            });
         }
         else {
             alert("Please enter a username and password.");
@@ -11,22 +28,29 @@ $(document).ready(function() {
     });
 });
 
-function submitForm() {
+function getData() {
     $.ajax({
-        type: "post",
-        url: "https://my.rochester.edu/webapps/login/index",
-        data: {
-            user_id: username,
-            login: "Login",
-            action: "login",
-            encoded_pw: window.btoa(password),
-            encoded_pw_unicode: "."
-        },
-        success: function() {
-          alert("success");
-        },
-        error: function() {
-          alert("error");
+        type: "get",
+        dataType: "html",
+        url: "https://my.rochester.edu/webapps/portal/frameset.jsp",
+        success: function(data) {
+            var $data = $(data);
+            var name = $("#global-nav-link", $data).text().trim();
+            name = name.substr(0,name.indexOf("Activity")).trim();
+            name = name.toLowerCase();
+            name = name.replace(/\b./g, function(m){ 
+                return m.toUpperCase(); 
+            });
+        }
+    });
+    $.ajax({
+        type: "get",
+        dataType: "html",
+        url: "https://ecard.sequoiars.com/rochester/eCardCardholder/StudentOverviewPage.aspx",
+        success: function(data) {
+          var $data = $(data);
+          var dataString = $(".balanceColumn").text().trim();
+          //To-Do: split string at $ to only get xx.xx. Right now looks like "Balance$ xx.xx$ xx.xx"
         }
     });
 }
