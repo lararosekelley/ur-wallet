12a8c6fb9b157cbfc6d2d3e0211f87f2e4c2b935
cwaldren <casey@fzy.me>
2013-12-09 21:14:52 -0500

    Updated UI and login code

--- popup.js
+++ popup.js
@@ -1,24 +1,45 @@
+jQuery.fn.exists = function(){return this.length>0;}
+
 $(document).ready(function() {
     $("#login").click(function(event) {
         event.preventDefault();
         if ($("#username").val() != "" && $("#password").val() != "") {
+            //Hide the button so we can display loader
+            $(this).hide();
+            $("#loader").show();
+
+
             var username = $("#username").val();
             var password = $("#password").val();
 
             var request = $.ajax({
                 type: "post",
-                dataType:"html",
+                dataType: "html",
                 url: "https://my.rochester.edu/webapps/login/index",
                 data: {
                     user_id: username,
                     encoded_pw: window.btoa(password),
                     encoded_pw_unicode: "."
                 },
-                success: function() {
-                    getData();
+                success: function(data) {
+                    var $data = $(data);
+                    console.log(data);
+                    if ($("#loginErrorMessage", $data).exists()) {
+                        $("#status").text("Wrong username or password.");
+                        $("input").css({"border":"1px solid red"});
+                        $("#loader").hide();
+                        $("#login").show();
+                    } else {
+                       
+
+                         $("#loader").hide();
+                        $("body").remove();
+
+                    }
                 },
                 error: function() {
-                    alert("Error! Try Again.");
+                    $("#loader").hide();
+                    $("#status").text("Failed to contact server. Try again later.");
                 }
             });
         }
@@ -28,13 +49,14 @@ $(document).ready(function() {
     });
 });
 
-function getData() {
+function checkValidLogin() {
     $.ajax({
         type: "get",
         dataType: "html",
         url: "https://my.rochester.edu/webapps/portal/frameset.jsp",
         success: function(data) {
             var $data = $(data);
+
             var name = $("#global-nav-link", $data).text().trim();
             name = name.substr(0,name.indexOf("Activity")).trim();
             name = name.toLowerCase();
