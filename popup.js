3a3e471e40c603d524a356ae4edd12531f07fc69
cwaldren <casey@fzy.me>
2013-12-10 00:27:18 -0500

    Huge update instead of english presentation
    Rewrote stuff as functions. now it seems to work. Uses saved data. Might
    as well just redownload all..

--- popup.js
+++ popup.js
@@ -1,73 +1,127 @@
 jQuery.fn.exists = function(){return this.length>0;}
+var storage = chrome.storage.sync;
 
-$(document).ready(function() {
-    $("#login").click(function(event) {
-        event.preventDefault();
-        if ($("#username").val() != "" && $("#password").val() != "") {
-            //Hide the button so we can display loader
-            $(this).hide();
-            $("#loader").show();
+function checkLoggedIn(data) {
+    console.log("Checking if user logged in")
+    if ($("#loginErrorMessage", data).exists()) {
+        console.log("wrong uname/pwd")
+        $("#status").text("Wrong username or password.");
+        $("input").css({"border":"1px solid red"});
+        $("#loader").hide();
+        $("#login").show();
+        return false;
+    }
+    else {
+        console.log("logged in !")
+       $("#loader").hide();
+       $("body").html("<h1>Logged In</h1>");
+       return true;
+    }
 
+}
 
-            var username = $("#username").val();
-            var password = $("#password").val();
-
+function loginWithSavedCredentials(callback) {
+    console.log("Trying to login with saved credentials")
+  
+    storage.get(["username", "password"], function(result) {
+        console.log(result)
+        if(result.username != null && result.password != null) {
             var request = $.ajax({
                 type: "post",
                 dataType: "html",
                 url: "https://my.rochester.edu/webapps/login/index",
                 data: {
-                    user_id: username,
-                    encoded_pw: window.btoa(password),
+                    user_id: result.username,
+                    encoded_pw: window.btoa(result.password),
                     encoded_pw_unicode: "."
                 },
                 success: function(data) {
+                    console.log("saved credentials ajax request sucess")
                     var $data = $(data);
-                    console.log(data);
-
-                    //Login failed
-                    if ($("#loginErrorMessage", $data).exists()) {
-                        $("#status").text("Wrong username or password.");
-                        $("input").css({"border":"1px solid red"});
-                        $("#loader").hide();
-                        $("#login").show();
-                    }
-                    //Login success! 
-                    else {
-                        $("#loader").hide();
-                        $("body").remove();
-
-                        //store credentials
-                        chrome.storage.sync.set({'username': username, 'password': password});
-
-                    }
+                    
+                    callback(checkLoggedIn($data));
                 },
                 error: function() {
-                    $("#loader").hide();
+                    console.log("saved credentials ajax request failed")
                     $("#status").text("Failed to contact server. Try again later.");
                 }
             });
-        }
-        else {
-            alert("Please enter a username and password.");
+
+
+        } else {
+            console.log("No credentials found")
+            callback(false)
         }
     });
+}
+
+$(document).ready(function() {
+
+    chrome.browserAction.setBadgeText({'text':"$999"});
+    $("#loader").show();
+    loginWithSavedCredentials(function(result) {
+        console.log("Waiting on results of loginWithSavedCredentials")
+        if (result){
+            console.log("logged in")
+            $("#loader").hide();
+        } 
+        else {
+            $("#loader").hide();
+            $("#loginform").show()
+            console.log("not logged in")
+            $("#login").click(function(event) {
+                event.preventDefault();
+                attemptToLoginUser();
+            });
+        }
+    })
+
 });
 
-function bb_login() {
-    //log into blackboard
-}
+function attemptToLoginUser() {
+    if ($("#username").val() != "" && $("#password").val() != "") {
+                //Hide the button so we can display loader
+                $("#login").hide();
+                $("#loader").show();
+                
+                var username = $("#username").val();
+                var password = $("#password").val();
 
-function seq_token() {
-    //return the sequioa token
-}
-
-function seq_login() {
-    //log into sequoia
-}
-
-function seq_info() {
-    //get this information
-    
+                var request = $.ajax({
+                    type: "post",
+                    dataType: "html",
+                    url: "https://my.rochester.edu/webapps/login/index",
+                    data: {
+                        user_id: username,
+                        encoded_pw: window.btoa(password),
+                        encoded_pw_unicode: "."
+                    },
+                    success: function(data) {
+                        var $data = $(data);
+                        //Login failed
+                        if ($("#loginErrorMessage", $data).exists()) {
+                            console.log("Ajax login request failed [wrong uname/pwd]")
+                            $("#status").text("Wrong username or password.");
+                            $("input").css({"border":"1px solid red"});
+                            $("#loader").hide();
+                            $("#login").show();
+                        }
+                        //Login success! 
+                        else {
+                            console.log("Ajax login request success")
+                            $("#loader").hide();
+                            $("body").html("<h1>Logged In</h1>");
+                            storage.set({'username':username, "password":password});
+                        }
+                    },
+                    error: function() {
+                        $("#loader").hide();
+                        $("#status").text("Failed to contact server. Try again later.");
+                    }
+                });
+            }
+            else {
+                $("#status").text("Please enter a username or password")
+            }
 }
 
