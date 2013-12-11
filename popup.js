ddd6f8f0c36925c0a21dc923636f0feb1d6a17b9
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2013-12-10 22:15:04 -0500

    Comments

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
+//JQuery Function
 jQuery.fn.exists = function(){return this.length>0;}
+
+//Chrome stores data on local storage
 var storage = chrome.storage.sync;
 
+
+//Check if User is already logged into BB
 function checkLoggedIn(data) {
     console.log("Checking if user logged in")
     if ($("#loginErrorMessage", data).exists()) {
@@ -14,12 +19,14 @@ function checkLoggedIn(data) {
     else {
         console.log("logged in !")
        $("#loader").hide();
-       $("body").html("<h1>Logged In</h1>");
+       $("body").html("<h1>Welcome, </h1>");
        return true;
     }
 
 }
 
+
+//If not, log in with saved username and password
 function loginWithSavedCredentials(callback) {
     console.log("Trying to login with saved credentials")
   
@@ -36,13 +43,13 @@ function loginWithSavedCredentials(callback) {
                     encoded_pw_unicode: "."
                 },
                 success: function(data) {
-                    console.log("saved credentials ajax request sucess")
+                    console.log("Saved credentials ajax request sucess")
                     var $data = $(data);
                     
                     callback(checkLoggedIn($data));
                 },
                 error: function() {
-                    console.log("saved credentials ajax request failed")
+                    console.log("Saved credentials ajax request failed")
                     $("#status").text("Failed to contact server. Try again later.");
                 }
             });
@@ -55,8 +62,8 @@ function loginWithSavedCredentials(callback) {
     });
 }
 
+//What to do after user fills out form
 $(document).ready(function() {
-
     chrome.browserAction.setBadgeText({'text':"$999"});
     $("#loader").show();
     loginWithSavedCredentials(function(result) {
@@ -78,6 +85,7 @@ $(document).ready(function() {
 
 });
 
+//Log in the user to BB for the first time
 function attemptToLoginUser() {
     if ($("#username").val() != "" && $("#password").val() != "") {
                 //Hide the button so we can display loader
@@ -101,7 +109,7 @@ function attemptToLoginUser() {
                         //Login failed
                         if ($("#loginErrorMessage", $data).exists()) {
                             console.log("Ajax login request failed [wrong uname/pwd]")
-                            $("#status").text("Wrong username or password.");
+                            $("#status").text("Oops! Wrong username or password.");
                             $("input").css({"border":"1px solid red"});
                             $("#loader").hide();
                             $("#login").show();
@@ -124,4 +132,3 @@ function attemptToLoginUser() {
                 $("#status").text("Please enter a username or password")
             }
 }
-
