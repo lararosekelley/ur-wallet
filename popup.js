5d6045ecf17dd8c3458aab6117afde5804c7400c
cwaldren <casey@fzy.me>
2013-12-11 02:00:20 -0500

    mother of all commits
    it works. Just load it up. wait about 6 sec

--- popup.js
+++ popup.js
@@ -4,22 +4,26 @@ jQuery.fn.exists = function(){return this.length>0;}
 //Chrome stores data on local storage
 var storage = chrome.storage.sync;
 
+//Constants
+var constants = {
+    FIRST_NAME : "",
+    LAST_NAME : "",
+    SEQUOIA_AUTH_TOKEN : "",
+    SEQUOIA_TOKEN_URL : "https://my.rochester.edu/webapps/bb-ecard-sso-bb_bb60/token.jsp",
+    SEQUOIA_AUTH_URL : "https://ecard.sequoiars.com/eCardServices/AuthenticationHandler.ashx",
+    SEQUOIA_BALANCE_URL : "https://ecard.sequoiars.com/eCardServices/eCardServices.svc/WebHttp/GetAccountHolderInformationForCurrentUser"
+}
 
 //Check if User is already logged into BB
 function checkLoggedIn(data) {
     console.log("Checking if user logged in")
     if ($("#loginErrorMessage", data).exists()) {
         console.log("wrong uname/pwd")
-        $("#status").text("Wrong username or password.");
-        $("input").css({"border":"1px solid red"});
-        $("#loader").hide();
-        $("#login").show();
+       
         return false;
     }
     else {
-        console.log("logged in !")
-       $("#loader").hide();
-       $("body").html("<h1>Welcome, </h1>");
+        console.log("logged in!")
        return true;
     }
 
@@ -31,7 +35,6 @@ function loginWithSavedCredentials(callback) {
     console.log("Trying to login with saved credentials")
   
     storage.get(["username", "password"], function(result) {
-        console.log(result)
         if(result.username != null && result.password != null) {
             var request = $.ajax({
                 type: "post",
@@ -45,7 +48,8 @@ function loginWithSavedCredentials(callback) {
                 success: function(data) {
                     console.log("Saved credentials ajax request sucess")
                     var $data = $(data);
-                    
+                    setupUserData($data);
+                    authenticateSequoia();
                     callback(checkLoggedIn($data));
                 },
                 error: function() {
@@ -69,11 +73,17 @@ $(document).ready(function() {
     loginWithSavedCredentials(function(result) {
         console.log("Waiting on results of loginWithSavedCredentials")
         if (result){
+            //We are in
             console.log("logged in")
             $("#loader").hide();
+            setupBBUI();
+           
         } 
         else {
+            $("#status").text("Wrong username or password.");
+            $("input").css({"border":"1px solid red"});
             $("#loader").hide();
+            $("#login").show();
             $("#loginform").show()
             console.log("not logged in")
             $("#login").click(function(event) {
@@ -120,6 +130,7 @@ function attemptToLoginUser() {
                             $("#loader").hide();
                             $("body").html("<h1>Logged In</h1>");
                             storage.set({'username':username, "password":password});
+
                         }
                     },
                     error: function() {
@@ -131,4 +142,69 @@ function attemptToLoginUser() {
             else {
                 $("#status").text("Please enter a username or password")
             }
+}
+
+function fetchRealName(data) {
+    var str = $("#global-nav-link", data).text().trim();
+    console.log(str)
+    var name = str.substr(0, str.indexOf("Activity")).trim();
+    return name;
+}
+
+function setupUserData(data) {
+    var name = fetchRealName(data).toLowerCase();
+    var firstName = name.split(" ")[0];
+    var lastName = name.split(" ")[1];
+    constants.FIRST_NAME = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+    constants.LAST_NAME = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+}
+
+function setupBBUI() {
+    $("#header h1").text("Welcome, " + constants.FIRST_NAME + ".")
+    $("#ui").show();
+}
+
+function authenticateSequoia() {
+ 
+    $.ajax({
+        type: "get",
+        dataType: "html",
+        url: constants.SEQUOIA_TOKEN_URL,
+        success: function(data) {
+            $data = $(data);
+            constants.SEQUOIA_AUTH_TOKEN = $("input[name='AUTHENTICATIONTOKEN']", $data).attr('value');
+        
+            $.ajax({
+                type: "post",
+                dataType: "html",
+                url: constants.SEQUOIA_AUTH_URL,
+                data: {
+                    AUTHENTICATIONTOKEN: constants.SEQUOIA_AUTH_TOKEN
+                },
+                success: function(data) {
+                    $.ajax({
+                        type: "post",
+                        dataType: "html",
+                        url: constants.SEQUOIA_BALANCE_URL,
+                        success: function(data) {
+                            $data = jQuery.parseJSON(data);
+                                var cash = $data.d._ItemList[1].BalanceInDollarsStr;
+                                var rawCash =$data.d._ItemList[1].BalanceInDollars;
+                         chrome.browserAction.setBadgeText({'text':cash});
+                         $("#declining").text(cash);
+                         if (rawCash < 10) {
+                            $("#declining").addClass("critical");
+                         } else if (rawCash > 10 && rawCash < 50 ) {
+                            $("#declining").addClass("semicritical");
+                         }else if (rawCash > 50 ) {
+                            $("#declining").addClass("healthy");
+                         }
+
+
+                        }
+                    });
+                }
+            });
+        }
+    });
 }
