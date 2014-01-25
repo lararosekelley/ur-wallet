96723d2b39ec03b0d0e0ebb0cc49030a7e7efdb0
cwaldren <casey@fzy.me>
2014-01-24 22:09:44 -0500

    add wallet mode

--- BetterBBChrome/controllers.js
+++ BetterBBChrome/controllers.js
@@ -2,17 +2,26 @@ var bbApp = angular.module('bbApp', ['bbAppControllers']);
 
 var bbAppControllers = angular.module('bbAppControllers', []);
 
+/* Holds variables common to all controllers */
 bbAppControllers.controller('MasterController',
-     ['$scope', '$timeout', '$http', '$location', function($scope, $timeout, $http, $location) {
-    //The mode of the page, login or main_view or something
+     ['$scope',  function($scope) {
+    //View modes:    
+    //pre_login   - asks user if they attend U of R
+    //login       - login form 
+    //wallet       - shows euros and declining
     $scope.mode = "pre_login";
 
-     $scope.uninstall = function() {
+    $scope.proceed = function() {
+        $scope.mode = "login";
+    }
+
+    $scope.uninstall = function() {
         chrome.management.uninstallSelf();
     }
 
 }]);
 
+/* Handles the logging in logic */
 bbAppControllers.controller('LoginController', 
     ['$scope', '$timeout', '$http', '$location', function($scope, $timeout, $http, $location) {
 
@@ -24,9 +33,21 @@ bbAppControllers.controller('LoginController',
 
     $scope.doLogin = function(form) {
         if (form.$valid) {
-            alert("yup")
+            $scope.$parent.mode = "wallet";
         }
     };
 
+   
+}]);
+
+
+/* Handles the wallet*/
+bbAppControllers.controller('WalletController', 
+    ['$scope', '$timeout', '$http', '$location', function($scope, $timeout, $http, $location) {
+
+   $scope.funds = {
+        euros: 0,
+        declining: 0
+   }
    
 }]);
