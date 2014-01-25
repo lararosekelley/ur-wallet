9dd3e77ee324d660e14b28c36ead5e28ece9b93a
cwaldren <casey@fzy.me>
2014-01-24 21:29:24 -0500

    Prompt to stop users who think it works elsewhere

--- BetterBBChrome/controllers.js
+++ BetterBBChrome/controllers.js
@@ -2,9 +2,31 @@ var bbApp = angular.module('bbApp', ['bbAppControllers']);
 
 var bbAppControllers = angular.module('bbAppControllers', []);
 
+bbAppControllers.controller('MasterController',
+     ['$scope', '$timeout', '$http', '$location', function($scope, $timeout, $http, $location) {
+    //The mode of the page, login or main_view or something
+    $scope.mode = "pre_login";
+
+     $scope.uninstall = function() {
+        chrome.management.uninstallSelf();
+    }
+
+}]);
+
 bbAppControllers.controller('LoginController', 
     ['$scope', '$timeout', '$http', '$location', function($scope, $timeout, $http, $location) {
 
-    $scope.mode = "login";
+   
+    $scope.user = {
+        netid: "",
+        password: "",
+    };
 
+    $scope.doLogin = function(form) {
+        if (form.$valid) {
+            alert("yup")
+        }
+    };
+
+   
 }]);
